3a76c60732cef8625349c7fa606f7e7172760e94
Jan Faracik <j.faracik@kainos.com>
2019-05-02 15:31:51 +0100

    Added a helper for nesting form data

--- helpers.py
+++ helpers.py
@@ -13,3 +13,17 @@ def get_next_form_after_pk(pk, section):
         if str(form.pk) == str(pk):
             next_one = True
     return
+
+
+def nest_data(sent_data):
+    data = {}
+
+    for q, v in sent_data.items():
+        nest_name = q.split('.')[0]
+
+        if nest_name not in data:
+            data[nest_name] = {}
+
+        data[nest_name][q.split('.')[1]] = v
+
+    return data
