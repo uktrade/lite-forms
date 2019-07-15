2cdcdf6863732382ac5de2a06941ba4b79be85e7
Jan Faracik <j.faracik@kainos.com>
2019-07-15 15:02:02 +0100

    Select now works correctly

--- templatetags/custom_tags.py
+++ templatetags/custom_tags.py
@@ -9,24 +9,33 @@ def key_value(dictionary, key):
 
 
 @register.filter
-def key_in_array(array, key):
-    # If array is empty, return False
-    if not array:
+def key_in_array(data, key):
+    # If data is empty, return False
+    if not data:
         return False
 
-    # If array is a list, check for the key
-    if isinstance(array, list):
-        for item in array:
+    if isinstance(data, str):
+        if data == key:
+            return True
+        return False
+
+    # If data is a dict, check the id
+    if isinstance(data, dict):
+        return data['id'] == key
+
+    # If data is a list, check for the key
+    if isinstance(data, list):
+        for item in data:
             if isinstance(item, str):
                 return item == key
 
             if item.get('id') == key:
                 return True
 
-        return key in array
+        return key in data
 
     # If else, loop through and check ids
-    return key in [x.get('id') for x in array]
+    return key in [x.get('id') for x in data]
 
 
 @register.filter
