7ff27cf7c2b05a75d22310cd29517b0704e6a00e
Jan Faracik <j.faracik@kainos.com>
2019-09-04 17:09:49 +0100

    Cleanup

--- lite_forms/submitters.py
+++ lite_forms/submitters.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest
 
 from lite_forms.components import HiddenField, Form, FormGroup
 from lite_forms.generators import form_page
-from lite_forms.helpers import remove_unused_errors, nest_data, get_next_form_after_pk, get_form_by_pk
+from lite_forms.helpers import remove_unused_errors, nest_data, get_next_form_after_pk, get_form_by_pk, flatten_data
 
 
 def submit_single_form(request: HttpRequest, form: Form, post_to, pk=None, override_data=None):
@@ -35,20 +35,31 @@ def submit_paged_form(request: HttpRequest, form_group: FormGroup, post_to, pk=N
 
     # Post the data to the validator and check for errors
     nested_data = nest_data(data)
+    nested_data = nest_data(data)
     if pk:
         validated_data, status_code = post_to(request, pk, nested_data)
     else:
         validated_data, status_code = post_to(request, nested_data)
 
     # If the API returns errors, add the existing questions to the reloaded form
-    errors = remove_unused_errors(validated_data.get('errors'), current_form)
-    if errors:
-        questions_to_add = []
-        for question in current_form.questions:
-            if hasattr(question, 'name') and errors.get(question.name):
-                questions_to_add.append(HiddenField(question.name, errors.get(question.name)))
+    errors = validated_data.get('errors')
 
-        current_form.questions.append(questions_to_add)
+    if errors:
+        errors = flatten_data(errors)
+        errors = remove_unused_errors(errors, current_form)
+
+    if errors:
+        for key, value in data.items():
+            exists = False
+
+            for question in current_form.questions:
+                if hasattr(question, 'name'):
+                    if question.name == key:
+                        exists = True
+                        continue
+
+            if not exists:
+                current_form.questions.insert(0, HiddenField(key, value))
 
         return form_page(request, current_form, data=data, errors=errors), validated_data
 
@@ -58,7 +69,7 @@ def submit_paged_form(request: HttpRequest, form_group: FormGroup, post_to, pk=N
 
     # Add existing post data to new form as hidden fields
     for key, value in data.items():
-        next_form.questions.append(HiddenField(key, value))
+        next_form.questions.insert(0, HiddenField(key, value))
 
     # Go to the next page
     return form_page(request, next_form), validated_data
