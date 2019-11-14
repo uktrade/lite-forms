14fef9a42818d60acfd6e83e4c1c32805b4a4387
Craig Anderson <craig.anderson@digital.trade.gov.uk>
2019-11-14 09:46:46 +0000

    Formatted master

--- styles.py
+++ styles.py
@@ -1,11 +1,11 @@
 class ButtonStyle:
-    DEFAULT = 'govuk-button'
-    SECONDARY = 'govuk-button govuk-button--secondary'
-    WARNING = 'govuk-button govuk-button--warning'
+    DEFAULT = "govuk-button"
+    SECONDARY = "govuk-button govuk-button--secondary"
+    WARNING = "govuk-button govuk-button--warning"
 
 
 class HeadingStyle:
-    XL = 'XL'
-    L = 'L'
-    M = 'M'
-    S = 'S'
+    XL = "XL"
+    L = "L"
+    M = "M"
+    S = "S"
--- views.py
+++ views.py
@@ -13,17 +13,18 @@ class FormView(TemplateView, ABC):
     Base class with properties necessary to support getting and posting to forms within subclasses of this
     class, using supplied data and actions.
     """
+
     data: dict = None
     action: callable = None
     object_pk = None
-    success_url: str = ''
+    success_url: str = ""
 
     def get_data(self):
         return self.data
 
     def get_action(self):
         if not self.action:
-            raise AttributeError('action has not been set')
+            raise AttributeError("action has not been set")
 
         return self.action
 
@@ -32,7 +33,7 @@ class FormView(TemplateView, ABC):
 
     def get_success_url(self):
         if not self.success_url:
-            raise AttributeError('success_url has not been set')
+            raise AttributeError("success_url has not been set")
 
         return self.success_url
 
@@ -40,18 +41,19 @@ class FormView(TemplateView, ABC):
         return self._validated_data
 
     def init(self, request, **kwargs):
-        raise NotImplementedError('init function not implemented')
+        raise NotImplementedError("init function not implemented")
 
 
 class SingleFormView(FormView):
     """
     Takes a single Form as a parameter and handles getting and posting to it using supplied values.
     """
+
     form: Form = None
 
     def get_form(self):
         if not self.form:
-            raise AttributeError('form has not been set')
+            raise AttributeError("form has not been set")
 
         return self.form
 
@@ -67,12 +69,16 @@ class SingleFormView(FormView):
         data = request.POST.copy()
 
         if self.get_object_pk():
-            validated_data, _ = self.get_action()(request, self.get_object_pk(), data)  # noqa
+            validated_data, _ = self.get_action()(
+                request, self.get_object_pk(), data
+            )  # noqa
         else:
             validated_data, _ = self.get_action()(request, data)  # noqa
 
-        if 'errors' in validated_data:
-            return form_page(request, self.get_form(), data=data, errors=validated_data.get('errors'))
+        if "errors" in validated_data:
+            return form_page(
+                request, self.get_form(), data=data, errors=validated_data.get("errors")
+            )
 
         self._validated_data = validated_data
 
@@ -83,11 +89,12 @@ class MultiFormView(FormView):
     """
     Takes a FormGroup as a parameter and handles getting and posting to forms in the group using supplied values.
     """
+
     forms: FormGroup = None
 
     def get_forms(self):
         if not self.forms:
-            raise AttributeError('form has not been set')
+            raise AttributeError("form has not been set")
 
         return self.forms
 
@@ -97,11 +104,15 @@ class MultiFormView(FormView):
     def get(self, request, **kwargs):
         self.init(request, **kwargs)
         form = self.get_forms().forms[0]
-        return form_page(request, form, data=self.get_data(), extra_data={'form_pk': form.pk})
+        return form_page(
+            request, form, data=self.get_data(), extra_data={"form_pk": form.pk}
+        )
 
     def post(self, request, **kwargs):
         self.init(request, **kwargs)
-        response, data = submit_paged_form(request, self.get_forms(), self.get_action(), object_pk=self.get_object_pk())
+        response, data = submit_paged_form(
+            request, self.get_forms(), self.get_action(), object_pk=self.get_object_pk()
+        )
 
         # If there are more forms to go through, continue
         if response:
