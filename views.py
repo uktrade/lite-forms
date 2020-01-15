a7a6de8786da300e334d1027a0ce86954a467b64
BenAndrew-Kainos <53567470+BenAndrew-Kainos@users.noreply.github.com>
2020-01-15 15:45:33 +0000

    inject data into multiformview (#41)
    feat: LT-1952

--- views.py
+++ views.py
@@ -124,7 +124,9 @@ class MultiFormView(FormView):
         self.init(request, **kwargs)
         self.on_submission(request, **kwargs)
 
-        response, data = submit_paged_form(request, self.get_forms(), self.get_action(), object_pk=self.get_object_pk())
+        response, data = submit_paged_form(
+            request, self.get_forms(), self.get_action(), object_pk=self.get_object_pk(), inject_data=self.get_data()
+        )
 
         # If there are more forms to go through, continue
         if response:
