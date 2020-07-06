a1d90e006a9b4750da26a45306fa40cb53786f4d
Andy Taylor <a.taylor@kainos.com>
2020-07-06 13:54:23 +0100

    bugfix: LT-2725 - Routing Rules Pagination (#114)

--- templatetags/custom_tags.py
+++ templatetags/custom_tags.py
@@ -191,8 +191,9 @@ def pagination_params(url, page):
     from urllib.parse import urlencode
 
     url_parts = list(urlparse.urlparse(url))
-    query = dict(urlparse.parse_qsl(url_parts[4]))
-    query.update({"page": page})
+    query_params = dict(urlparse.parse_qsl(url_parts[4]))
+    query_params.pop("page", 1)
+    query = {"page": page, **query_params}
     url_parts[4] = urlencode(query)
 
     return urlparse.urlunparse(url_parts)
